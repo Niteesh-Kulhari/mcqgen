288329ab6e101a6f976ada8be2a7596d32db504c
Niteesh Kulhari <nkulhari96@gmail.com>
2024-03-27 17:06:57 -0400

    Added more code

---
+++ src/mcqgenerator/utils.py
@@ -0,0 +1,40 @@
+import os
+import PyPDF2
+import json
+import traceback
+
+def read_file(file):
+    if file.name.endswith(".pdf"):
+        try:
+            pdf_reader = PyPDF2.PdfFileReader(file)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+            return text
+        except Exception as e:
+            raise Exception("Error while reading the pdf file")
+    
+    elif file.name.endswith(".txt"):
+        return file.read().decode("utf-8")
+    
+    else:
+        raise Exception("Unsupported File Format")
+    
+
+def get_table_data(quiz_str):
+    try:
+        quiz_dict = json.loads(quiz_str)
+        quiz_table_data = []
+
+        for key,value in quiz_dict.item():
+            mcq = value["mcq"]
+            options = " || ".join(
+                [
+                    f"{option}-> {option_value}" for option, option_value in value["options"].item()
+                ]
+            )
+            correct=value["correct"]
+            quiz_table_data.append({"MCQ":mcq,"Choices":options,"Correct":correct})
+        return quiz_table_data
+    except:
+        raise Exception
